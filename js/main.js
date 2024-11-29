f72bd452cd39f9c1383f59feda6a654297dabe58
Lele <nicolas.lcorbalan@davinci.edu.ar>
2024-11-28 23:53:45 -0300

    Entrega parcial 2 parte 2. Versión 1.0

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let orden = 0;
+const $bsModal = new bootstrap.Modal('#productDetail');
+
+// Selecciono todos los productos, y los recorro con forEach para agregarles un evento de tecldo
+document.querySelectorAll(".product").forEach(($card) => {
+    $card.addEventListener("keydown", (ev) => {
+        if(ev.keyCode === 13 || ev.keyCode === 32) {
+            $bsModal.show()
+        }
+    })
+})
+
+document.querySelector("#btnBanner1").addEventListener("click", ()=> {
+    alert("Por ahora no se puede, vuelva más tarde :)");
+})
+
+document.querySelector("#btnBanner3").addEventListener("click", ()=> {
+    $bsModal.show()
+})
+
+document.querySelectorAll(".product .btn").forEach(($card) => {
+    $card.addEventListener("click", (ev) => {
+        console.log("Agregar al carrito...");
+        ev.stopPropagation();
+    })
+})
+
+// FONDO
+const imgRuta = "../img/hero.webp";
+const imgWidth = 1920;
+const imgHeight = 1280;
+
+window.addEventListener("resize", () => {
+    // document.body.style.backgroundSize = '';
+})
+
+// TEST
+// document.addEventListener("DOMContentLoaded", () => { $bsModal.show() })
